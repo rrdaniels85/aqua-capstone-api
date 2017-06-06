d1ec7506c556ffc6cd8e92c70921a3ad00f5c320
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-06-06 10:13:42 -0400

    Updates routes file for animals
    Updates routes file in config. Adds animals underneath tanks route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :animals
-  resources :tanks
+  resources :tanks, except: [:new, :edit] do
+  resources :animals, except: [:new, :edit, :show]
+  end
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
